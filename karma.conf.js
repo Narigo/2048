897c193e2e5d1ebc9903d678a06d703587fa1538
Joern Bernhardt <jb@campudus.com>
2015-11-24 20:03:13 +0100

    Add coverage
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- karma.conf.js
+++ karma.conf.js
@@ -30,11 +30,14 @@ module.exports = function(config) {
       'test/**/*.js' : [ 'browserify' ]
     },
 
+    browserify: {
+      transform: ['browserify-istanbul']
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
